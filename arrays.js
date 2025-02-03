f92b283a2838137b114fdd2f84de905c8ba362af
Katrinerosa <katrinerosa@gmail.com>
2025-02-03 09:34:43 +0100

    2.opgave

--- arrays.js
+++ arrays.js
@@ -30,12 +30,36 @@ document.addEventListener("DOMContentLoaded", function () {
   let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21];
 
   // --- Løs opgaverne herunder ---
-  let newNumbers = numbersArray.filter(function(number){ 
-    return number > 20 
-  })
-    console.log("Opgave 1", newNumbers);
+  let newNumbers = numbersArray.filter(function (number) {
+    return number > 20;
+  });
+  console.log("Opgave 1", newNumbers);
 
-    // --opgave 2--
+  // --opgave 2--
+  
+
+  console.log(catsArray.length);
+  console.log("Opgave 2");
+  // Expected output: 4
+  
+  /* let catsArray = [
+    "Ollie",
+    "snifles",
+    "Sophie",
+    "Salem",
+    "Tiger",
+    "Binx",
+    "Pumpkin",
+    "Penny",
+    "Lenny",
+  ];
+  console.log("Opgave 2"); */
+
+
+  //--opgave3--
+
+  console.log("Opgave 3a", ...)
+
 
 
 }); // ends DOMContentLoaded
